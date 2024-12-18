2398a25d694c41a3ff21ffc9b05104466fb0798c
anujYadav <anujvns28@gmail.com>
2024-12-18 10:52:46 +0530

    updating cookie opetion

--- controller/auth.js
+++ controller/auth.js
@@ -109,8 +109,13 @@ exports.login = async (req, res) => {
 
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
       };
+      
 
+      
       res.cookie("token", token, options).status(200).json({
         success: true,
         user,
